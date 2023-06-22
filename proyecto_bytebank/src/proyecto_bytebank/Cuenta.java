fb27717a47a7d8771ca5f61bdbddaeea9499c740
Anthony Moises Condori Choccata <113246456+Alt-Anthony@users.noreply.github.com>
2023-06-22 12:00:25 -0500

    prueba de metodos

--- proyecto_bytebank/src/proyecto_bytebank/Cuenta.java
+++ proyecto_bytebank/src/proyecto_bytebank/Cuenta.java
@@ -1,10 +1,31 @@
 package proyecto_bytebank;
 
 class Cuenta {
-		
-		double saldo;
-		int agencia;
-		int numero;
-		String titular;
-		
+	double saldo;
+	int agencia;
+	int numero;
+	String titular;
+
+	public void depositar(double valor) {
+		this.saldo += valor;
+	}
+
+	public boolean retirar(double saldo) {
+		if (this.saldo >= saldo) {
+			this.saldo -= saldo;
+			return true;
+		}
+		return false;
+	}
+
+	public boolean transferir(double valor, Cuenta cuentaaTransferir) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			cuentaaTransferir.depositar(valor);
+			return true;
+		} else {
+			return false;
+		}
+
+	}
 }
