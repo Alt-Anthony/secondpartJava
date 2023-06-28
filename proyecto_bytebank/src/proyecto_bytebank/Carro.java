8b141f544fa3b39fb5a2a950672afe097c98b5c4
Anthony Moises Condori Choccata <113246456+Alt-Anthony@users.noreply.github.com>
2023-06-27 23:55:30 -0500

    constructor examples

---
+++ proyecto_bytebank/src/proyecto_bytebank/Carro.java
@@ -0,0 +1,21 @@
+package proyecto_bytebank;
+
+public class Carro{
+    private int ano;
+    private String modelo;
+    private double precio;
+    
+    public Carro(int ano, String modelo, double precio) {
+        this.ano = ano;
+        this.modelo = modelo;
+        this.precio = precio;
+    }
+    public Carro(String modelo, double precio){
+        this.ano = 2017;
+        this.modelo = modelo;
+        this.precio = precio;
+    }
+
+    //getters e setters omitidos        
+
+}
---
+++ proyecto_bytebank/src/proyecto_bytebank/PruebaCarro.java
@@ -0,0 +1,10 @@
+package proyecto_bytebank;
+
+public class PruebaCarro {
+	public static void main(String[] args) {
+		
+		Carro carro = new Carro(2013, "Gol", 35000.0);
+		
+		Carro carro2 = new Carro("Uno", 2000.0);
+	}
+}
