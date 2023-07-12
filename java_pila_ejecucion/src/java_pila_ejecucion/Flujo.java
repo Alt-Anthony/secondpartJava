66b4901d8de5c73f049d845e641b4681c2dabdd2
Anthony Moises Condori Choccata <113246456+Alt-Anthony@users.noreply.github.com>
2023-07-12 12:14:10 -0500

    debug y atrapar excepciones

--- java_pila_ejecucion/src/java_pila_ejecucion/Flujo.java
+++ java_pila_ejecucion/src/java_pila_ejecucion/Flujo.java
@@ -18,6 +18,22 @@ public class Flujo {
 		System.out.println("Incio metodo2");
 		for (int i = 1; i <= 5; i++) {
 			System.out.println("i");
+			try { // Intenta correr esto
+				if (i == 3) {
+					int num = 0;																																			
+					int resultado = i / num;
+					System.out.println(resultado);
+				}
+
+				String test = null;
+				System.out.println(test.toString());
+
+			} catch (ArithmeticException | NullPointerException exception) { // Si es asi, atrapa este error
+				System.out.println("Atrapo exception");
+				System.out.println(exception.getMessage());
+				exception.printStackTrace();
+			}
+
 		}
 		System.out.println("Fin metodo2");
 	}
